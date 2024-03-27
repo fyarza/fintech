628e91498105ad32f0161ea311bdb6654f93ec86
Federico Yarza <federicoyarza295@gmail.com>
2024-03-26 20:23:54 -0400

    Initial Commit

---
+++ app/index.tsx
@@ -0,0 +1,89 @@
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import { useAssets } from "expo-asset";
+import { ResizeMode, Video } from "expo-av";
+import { Link } from "expo-router";
+import { defaultStyles } from "@/constants/Styles";
+import Colors from "@/constants/Colors";
+
+const Page = () => {
+  const [assets] = useAssets([require("@/assets/videos/intro.mp4")]);
+  return (
+    <View style={styles.container}>
+      {assets && (
+        <Video
+          shouldPlay
+          resizeMode={ResizeMode.COVER}
+          isLooping
+          isMuted
+          source={{ uri: assets[0].uri }}
+          style={styles.video}
+        />
+      )}
+      <View style={{ marginTop: 80, padding: 20 }}>
+        <Text style={styles.header}>Ready to change the way you money?</Text>
+      </View>
+      <View style={styles.buttons}>
+        <Link
+          href={"/login"}
+          style={[
+            defaultStyles.pillButton,
+            {
+              flex: 1,
+              backgroundColor: Colors.dark,
+            },
+          ]}
+          asChild
+        >
+          <TouchableOpacity>
+            <Text style={{ color: "white", fontSize: 22, fontWeight: "500" }}>
+              Log in
+            </Text>
+          </TouchableOpacity>
+        </Link>
+        <Link
+          href={"/signup"}
+          style={[
+            defaultStyles.pillButton,
+            {
+              flex: 1,
+              backgroundColor: "#fff",
+            },
+          ]}
+          asChild
+        >
+          <TouchableOpacity>
+            <Text style={{ fontSize: 22, fontWeight: "500" }}>Sign Up</Text>
+          </TouchableOpacity>
+        </Link>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  video: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+  },
+  header: {
+    fontSize: 36,
+    color: "white",
+    fontWeight: "900",
+    textTransform: "uppercase",
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 20,
+    padding: 20,
+    marginBottom: 60,
+  },
+});
+
+export default Page;
---
+++ app/verify/[phone].tsx
@@ -0,0 +1,130 @@
+import Colors from "@/constants/Colors";
+import { defaultStyles } from "@/constants/Styles";
+import { useSignIn, useSignUp } from "@clerk/clerk-expo";
+import { Link, useLocalSearchParams } from "expo-router";
+import React, { Fragment, useEffect, useState } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import {
+  CodeField,
+  Cursor,
+  useBlurOnFulfill,
+  useClearByFocusCell,
+} from "react-native-confirmation-code-field";
+
+const CELL_COUNT = 6;
+
+function Page() {
+  const { phone, signin } = useLocalSearchParams<{
+    phone: string;
+    signin?: string;
+  }>();
+  const [code, setCode] = useState("");
+
+  const { signIn } = useSignIn();
+  const { signUp, setActive } = useSignUp();
+
+  const ref = useBlurOnFulfill({ value: code, cellCount: CELL_COUNT });
+  const [props, getCellOnLayoutHandler] = useClearByFocusCell({
+    value: code,
+    setValue: setCode,
+  });
+
+  useEffect(() => {
+    if (code.length === 6) {
+      console.log("code", code);
+      if (signin === "true") {
+        verifySigIn();
+      } else {
+        verifyCode();
+      }
+    }
+  }, [code]);
+
+  const verifyCode = async () => {};
+  const verifySigIn = async () => {};
+  return (
+    <View style={defaultStyles.containter}>
+      <Text style={defaultStyles.header}>6-digit code</Text>
+      <Text style={defaultStyles.descriptionText}>
+        Code sent to {phone} unless you already have an account
+      </Text>
+
+      <CodeField
+        ref={ref}
+        {...props}
+        value={code}
+        onChangeText={setCode}
+        cellCount={CELL_COUNT}
+        rootStyle={styles.codeFieldRoot}
+        keyboardType="number-pad"
+        textContentType="oneTimeCode"
+        renderCell={({ index, symbol, isFocused }) => (
+          <Fragment key={index}>
+            <View
+              onLayout={getCellOnLayoutHandler(index)}
+              key={index}
+              style={[styles.cellRoot, isFocused && styles.focusCell]}
+            >
+              <Text style={styles.cellText}>
+                {symbol || (isFocused ? <Cursor /> : null)}
+              </Text>
+            </View>
+            {index === 2 ? (
+              <View key={`separator-${index}`} style={styles.separator} />
+            ) : null}
+          </Fragment>
+        )}
+      />
+      <Link href={"/login"} replace asChild>
+        <TouchableOpacity></TouchableOpacity>
+      </Link>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  codeFieldRoot: {
+    marginVertical: 20,
+    marginLeft: "auto",
+    marginRight: "auto",
+    gap: 12,
+  },
+  cellRoot: {
+    width: 45,
+    height: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.lightGray,
+    borderRadius: 8,
+  },
+  cellText: {
+    color: "#000",
+    fontSize: 36,
+    textAlign: "center",
+  },
+  separator: {
+    height: 2,
+    width: 10,
+    backgroundColor: Colors.gray,
+    alignSelf: "center",
+  },
+  cell: {
+    width: 40,
+    height: 40,
+    lineHeight: 38,
+    fontSize: 24,
+    borderWidth: 2,
+    borderColor: "#00000030",
+    textAlign: "center",
+  },
+  focusCell: {
+    borderColor: "#000",
+  },
+});
+
+export default Page;
